c25f3123d70ed16584485447dc813a35295abd94
NRGrus <NRGrus>
2019-06-03 13:00:49 +0600

    lesson

--- src/app/pages/lesson/lesson.page.ts
+++ src/app/pages/lesson/lesson.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
+import {DatePipe} from '@angular/common';
 
 interface Iuser {
   id: number;
@@ -35,6 +36,11 @@ interface Ilesson {
   room: string;
   teacher: Iuser;
 }
+interface Iattendance {
+  id: number;
+  student: Iuser;
+  is_exist: boolean;
+}
 @Component({
   selector: 'app-lesson',
   templateUrl: './lesson.page.html',
@@ -42,11 +48,15 @@ interface Ilesson {
 })
 export class LessonPage implements OnInit {
   id: number;
+  user: Iuser;
   lesson: Ilesson;
+  attendance: Iattendance[];
+
   constructor(
       private route: ActivatedRoute,
       private auth: AuthService,
-      private http: HttpClient
+      private http: HttpClient,
+      private datePipe: DatePipe
   ) { }
 
   ngOnInit() {
@@ -54,8 +64,14 @@ export class LessonPage implements OnInit {
         .subscribe((params: any) => {
           this.id = params.id;
         });
+    this.auth.authUser.subscribe((res: Iuser) => {
+      this.user = res;
+    });
     this.getLessonData().subscribe((res: Ilesson) => {
       this.lesson = res;
+    });
+    this.getAttendance().subscribe((res: Iattendance[]) => {
+      this.attendance = res;
     });
   }
 
@@ -76,5 +92,28 @@ export class LessonPage implements OnInit {
           throw new Error(e);
         })
     );
+  }
+
+  getAttendance() {
+    let header = new HttpHeaders();
+    header = header.append('Content-Type', 'application/json');
+    header = header.append('Authorization', `Bearer ${this.auth.token}`);
+    return this.http.get(`${this.auth.url}/api/igra/attendance/${this.id}`, {headers: header}).pipe(
+        map((res: any) => {
+          return res.data;
+        }),
+        catchError(e => {
+          const status = e.status;
+          if (status === 401) {
+            this.auth.showAlert('You are not authorized for this!');
+            this.auth.logout();
+          }
+          throw new Error(e);
+        })
+    );
+  }
+
+  getDateString(lessonDate: string) {
+    return this.datePipe.transform(new Date(lessonDate), 'MMM d');
   }
 }
