7d0283a4c11250225651b066214507eb9feaaf52
Ruslan <nrgruslan@gmail.com>
2019-06-10 18:25:26 +0600

    finance salary

--- src/app/pages/finance/finance.page.ts
+++ src/app/pages/finance/finance.page.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {LoadingController} from '@ionic/angular';
+import {catchError, map} from 'rxjs/operators';
 
+interface Iuser {
+  id: number;
+  email: string;
+  surname: string;
+  name: string;
+  gender: boolean;
+  address: string;
+  image: string;
+  birthday: string;
+  phone_number: string;
+}
+interface Isalary {
+  total: number;
+  lesson: number;
+  bonus: number;
+  fine: number;
+}
 @Component({
   selector: 'app-finance',
   templateUrl: './finance.page.html',
   styleUrls: ['./finance.page.scss'],
 })
 export class FinancePage implements OnInit {
-
-  constructor() { }
+  user: Iuser;
+  salary: Isalary;
+  constructor(
+      private route: ActivatedRoute,
+      private auth: AuthService,
+      private http: HttpClient,
+      private loadingController: LoadingController
+  ) { }
 
   ngOnInit() {
+    this.getData();
   }
 
+  async getData() {
+    const loading = await this.loadingController.create({
+      spinner: 'crescent',
+      message: 'Please wait...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present().then(() => {
+      this.auth.authUser.subscribe((res: Iuser) => {
+        this.user = res;
+      });
+      this.getSalaryData().subscribe((res: Isalary) => {
+        this.salary = res;
+        loading.dismiss();
+      });
+    });
+  }
+  getSalaryData() {
+    let header = new HttpHeaders();
+    header = header.append('Content-Type', 'application/json');
+    header = header.append('Authorization', `Bearer ${this.auth.token}`);
+    return this.http.get(`${this.auth.url}/api/igra/get-salary`, {headers: header}).pipe(
+        map((res: any) => {
+          return res;
+        }),
+        catchError(e => {
+          const status = e.status;
+          if (status === 401) {
+            this.auth.showAlert('You are not authorized for this!');
+            this.auth.logout();
+          }
+          throw new Error(e);
+        })
+    );
+  }
 }
